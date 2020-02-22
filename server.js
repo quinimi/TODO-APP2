4a12c2c5988bb966b897db445d1432736094d932
Quin Mixon <qmixon@live.com>
2020-02-21 22:19:21 -0800

    Connected Node.js app to Mongo db

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 let express = require('express')
+let mongodb = require('mongodb')
 
 let app = express()
+let db 
+
+let connectionString = 'mongodb+srv://todoAppUser:love1234@cluster0-ahdmu.mongodb.net/todoApp?retryWrites=true&w=majority'
+mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+    db = client.db()
+    app.listen(3000)
+})
 
 app.use(express.urlencoded({extended: false}))
 
@@ -59,8 +67,9 @@ app.get('/', function(req, res) {
 })
 
 app.post('/create-item', function(req, res) {
-    console.log(req.body.item)
+   db.collection('items').insertOne({text: req.body.item}, function(){
     res.send("Thanks for submitting the form.")
+   })
+    
 })
 
-app.listen(3000)
